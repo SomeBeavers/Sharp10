52105b889a5f24013f955abeeb270f4e1956c11d
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2021-07-23 13:21:58 +0300

    Add ParameterlessStruct.cs

---
+++ Sharp10/ParameterlessStruct.cs
@@ -0,0 +1,35 @@
+﻿namespace Sharp10
+{
+    public struct ParameterlessStruct
+    {
+        private string Name;
+        private string Nickname = "s";
+
+        public ParameterlessStruct()
+        {
+        }
+
+        public override string ToString()
+        {
+            return GetType().ToString();
+        }
+    }
+    public class ParameterlessClass
+    {
+        private string Name;
+        private string Nickname = "s";
+
+        public ParameterlessClass()
+        {
+        }
+    }
+
+    public class UseStruct
+    {
+        private void Test()
+        {
+            new ParameterlessStruct(){};
+            ParameterlessStruct parameterlessStruct = new(){};
+        }
+    }
+}
