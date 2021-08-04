fb4ef269faa9d965dcc31b1a8b9032c985f97618
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2021-08-04 16:06:44 +0300

    Add LambdaImprovements.cs.

---
+++ Sharp10/LambdaImprovements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sharp10
+{
+    public class LambdaImprovements
+    {
+        [A]
+        private void Test1(out Func<string, string> f)
+        {
+            //f = [A]() => { return ""; }; 
+            f = ([A] x)=> x; // [A] x
+        }
+
+        [A]
+        private void Test2(out Func<string> f)
+        {
+            f = [A]() => { return ""; };
+        }
+
+        [A]
+        private void Test3(out Func<int, int> f)
+        {
+            f = int(int x) => x; // ok
+            int t = f(1);
+        }
+    }
+
+    internal class AAttribute : Attribute
+    {
+    }
+}
