6f43817f0595d38cd48088239a3ff73318ee1879
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2021-08-13 14:46:46 +0300

    Add playground.

---
+++ FileScopedNamespace/Playground/Class1.cs
@@ -0,0 +1,22 @@
+﻿// Sharp10 FileScopedNamespace Class1.cs
+
+namespace FileScopedNamespace.Playground;
+
+public class Class1 : IClass1
+{
+    public void Test(AA a, bool b)
+    {
+        if (a.isTrue || a.isTrue)
+        {
+        }
+
+        if (b || b)
+        {
+        }
+    }
+}
+
+public class AA
+{
+    public bool isTrue { get; set; }
+}
---
+++ FileScopedNamespace/Playground/IClass1.cs
@@ -0,0 +1,8 @@
+﻿// Sharp10 FileScopedNamespace IClass1.cs
+
+namespace FileScopedNamespace.Playground;
+
+public interface IClass1
+{
+    void Test(AA a, bool b);
+}
